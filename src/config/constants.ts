32e07fd3f0d712608d6bf3eaa01f615a2fa4204d
jitin <jitinrathi910949@gmail.com>
2021-05-04 02:15:22 +0530

    4 digit otp

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 import config from 'config';
 import { S3Config, TwilioConfig } from '../interfaces/config.interface';
 
-export const defaultCodeLength = 6 | 4;
+export const defaultCodeLength = 4;
 
 export const twilioConfig: TwilioConfig = {
   ACC_ID: config.get('ACCOUNT_SID'),
