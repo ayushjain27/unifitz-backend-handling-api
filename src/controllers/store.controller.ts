3052cc3ea1d22b98716e7f39504efe2e46b081e1
jitin <jitinrathi910949@gmail.com>
2021-05-11 22:44:39 +0530

    controller logs added

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -87,6 +87,9 @@ export class StoreController {
   };
   uploadFile = async (req: Request, res: Response) => {
     const storeDocUploadRequest: StoreDocUploadRequest = req.body;
+    console.log("---------------------");
+    console.log("req body is", req.body);
+    console.log("---------------------");
     Logger.info(
       '<Controller>:<StoreController>:<Upload file request controller initiated>'
     );
