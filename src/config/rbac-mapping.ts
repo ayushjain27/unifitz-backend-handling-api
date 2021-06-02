cc0a2654b36745e2664b6f78cfc7834d6a13a653
jitin <jitinrathi910949@gmail.com>
2021-06-03 00:47:36 +0530

    get permission given to store owner

--- src/config/rbac-mapping.ts
+++ src/config/rbac-mapping.ts
@@ -3,6 +3,6 @@ import { ACL } from '../enum/rbac.enum';
 /* eslint-disable */
 export const RBAC_MAP: any = {
   admin: [ACL.STORE_CREATE, ACL.STORE_GET],
-  store_owner: [ACL.STORE_CREATE, ACL.STORE_GET_OWNER],
+  store_owner: [ACL.STORE_GET, ACL.STORE_CREATE, ACL.STORE_GET_OWNER],
   user: [ACL.STORE_GET, ACL.STORE_REVIEW_CREATE]
 };
