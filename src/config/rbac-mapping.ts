fd7c31a1585faee7aa59e5e3b58ffe791fd15538
JAGJEET SINGH <jshsingh.jsh@ggmail.com>
2021-06-03 09:32:29 +0530

    get all stores api created

--- src/config/rbac-mapping.ts
+++ src/config/rbac-mapping.ts
@@ -2,7 +2,7 @@ import { ACL } from '../enum/rbac.enum';
 
 /* eslint-disable */
 export const RBAC_MAP: any = {
-  admin: [ACL.STORE_CREATE, ACL.STORE_GET],
-  store_owner: [ACL.STORE_GET, ACL.STORE_CREATE, ACL.STORE_GET_OWNER],
-  user: [ACL.STORE_GET, ACL.STORE_REVIEW_CREATE]
+  admin: [ACL.STORE_CREATE, ACL.STORE_GET_ALL, ACL.STORE_GET_SINGLE],
+  store_owner: [ACL.STORE_CREATE, ACL.STORE_GET_OWNER, ACL.STORE_GET_SINGLE],
+  user: [ACL.STORE_GET_ALL, ACL.STORE_REVIEW_CREATE, ACL.STORE_GET_SINGLE]
 };
