91bf6175e97e0c17d7e1b6e7732ccfccf6ee23a1
JAGJEET SINGH <jshsingh.jsh@ggmail.com>
2021-06-03 09:32:29 +0530

    get all stores api created

--- src/enum/rbac.enum.ts
+++ src/enum/rbac.enum.ts
@@ -1,6 +1,7 @@
 export enum ACL {
   STORE_CREATE = 'store:write',
-  STORE_GET = 'store:read',
+  STORE_GET_ALL = 'store:read_all',
   STORE_GET_OWNER = 'store:read_owner',
+  STORE_GET_SINGLE = 'store:read_single',
   STORE_REVIEW_CREATE = 'store_review:create'
 }
