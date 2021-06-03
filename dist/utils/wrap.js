79234fdee113f5a9bc73ad49bca568a78a57099d
JAGJEET SINGH <jshsingh.jsh@ggmail.com>
2021-06-03 09:36:00 +0530

    removed dist

--- dist/utils/wrap.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.wrap = void 0;
-/* eslint-disable */
-const wrap = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-};
-exports.wrap = wrap;
-//# sourceMappingURL=wrap.js.map
