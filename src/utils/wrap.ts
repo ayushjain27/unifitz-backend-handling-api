bb4bd66a3c80d8c5cac5006bf671f5083c0d3d36
JAGJEET SINGH <jshsingh.jsh@ggmail.com>
2021-06-04 22:02:11 +0530

    added logi api - admin

--- src/utils/wrap.ts
+++ src/utils/wrap.ts
@@ -1,9 +1,6 @@
 import { NextFunction } from 'express';
 /* eslint-disable */
-export const wrap = (fn: any) => (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-};
+export const wrap =
+  (fn: any) => (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  };
