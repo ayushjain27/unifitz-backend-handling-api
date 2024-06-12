9f3259b76c97f700c276cf65efeb31d5509f5779
AayushJain27 <ayush@serviceplug.in>
2024-06-12 17:24:41 +0530

    modify

--- src/config/permissions.ts
+++ src/config/permissions.ts
@@ -1,126 +1,186 @@
 export const permissions: any = {
     EMPLOYEE: {
         DASHBOARD: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        ANALYTICS_CUSTOMER: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        ADVERTISEMENT_AND_PLUS_FEATURE: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        STORES: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        CUSTOMER: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        VEHICLES_OWNED: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        VEHICLES_BUY_SELL: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        PRODUCT_AND_SERVICES: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        PRELIST_PRODUCT_AND_SERVICES: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        B2B_PARTNER_PRODUCT: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        CATEGORY: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        ADVERTIISEMENT: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        EVENT: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        OFFER: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        BUSINESS_OPPORTUNITIES: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        SCHOOL_OF_AUTO: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        B2B_PARTNER: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        REPORT: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        REPORT_NOTES: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        },
-        RATING_AND_REVIEWS: {
-            CREATE: true,
-            READ: true,
-            UPDATE: true,
-            DELETE: true
-        }
-    },
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       ANALYTICS_CUSTOMER: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       ADVERTISEMENT_AND_PLUS_FEATURE: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       ANALYTICS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       STORES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       CUSTOMER: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       VEHICLES_OWNED: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       VEHICLES_BUY_SELL: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PRODUCT_AND_SERVICES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PRELIST_PRODUCT_AND_SERVICES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       B2B_PARTNER_PRODUCT: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       CATEGORY: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+    //    ADVERTIISEMENT: {
+    //        CREATE: true,
+    //        READ: true,
+    //        UPDATE: true,
+    //        DELETE: true
+    //    },
+       PLUS_FEATURES_EVENT: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PLUS_FEATURES_OFFERS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PLUS_FEATURES_BUSINESS_OPPORTUNITIES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PLUS_FEATURES_SCHOOL_OF_AUTO: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       OFFERS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       B2B_PARTNERS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       REPORT: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       REPORT_NOTES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       RATING_AND_REVIEWS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PRIVILEDGE: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       SP_EMPLOYEE: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       USER_PROFILE: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PLUS_FEATURES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       ADVERTISEMENTS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       PRODUCTS: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       VEHICLES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+       NEW_VEHICLES: {
+           CREATE: true,
+           READ: true,
+           UPDATE: true,
+           DELETE: true
+       },
+   },
     OEM: {
          DASHBOARD: {
             CREATE: true,
@@ -140,6 +200,12 @@ export const permissions: any = {
             UPDATE: true,
             DELETE: true
         },
+        ANALYTICS: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
         STORES: {
             CREATE: true,
             READ: true,
@@ -188,37 +254,43 @@ export const permissions: any = {
             UPDATE: true,
             DELETE: true
         },
-        ADVERTIISEMENT: {
+        // ADVERTIISEMENT: {
+        //     CREATE: true,
+        //     READ: true,
+        //     UPDATE: true,
+        //     DELETE: true
+        // },
+        PLUS_FEATURES_EVENT: {
             CREATE: true,
             READ: true,
             UPDATE: true,
             DELETE: true
         },
-        EVENT: {
+        PLUS_FEATURES_OFFERS: {
             CREATE: true,
             READ: true,
             UPDATE: true,
             DELETE: true
         },
-        OFFER: {
+        PLUS_FEATURES_BUSINESS_OPPORTUNITIES: {
             CREATE: true,
             READ: true,
             UPDATE: true,
             DELETE: true
         },
-        BUSINESS_OPPORTUNITIES: {
+        PLUS_FEATURES_SCHOOL_OF_AUTO: {
             CREATE: true,
             READ: true,
             UPDATE: true,
             DELETE: true
         },
-        SCHOOL_OF_AUTO: {
+        OFFERS: {
             CREATE: true,
             READ: true,
             UPDATE: true,
             DELETE: true
         },
-        B2B_PARTNER: {
+        B2B_PARTNERS: {
             CREATE: true,
             READ: true,
             UPDATE: true,
@@ -241,7 +313,55 @@ export const permissions: any = {
             READ: true,
             UPDATE: true,
             DELETE: true
-        }
+        },
+        PRIVILEDGE: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        SP_EMPLOYEE: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        USER_PROFILE: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        PLUS_FEATURES: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        ADVERTISEMENTS: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        PRODUCTS: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        VEHICLES: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
+        NEW_VEHICLES: {
+            CREATE: true,
+            READ: true,
+            UPDATE: true,
+            DELETE: true
+        },
     },
 }
 
