6438c5245519da0124abf773523de057aa55b783
AayushJain27 <ayush@serviceplug.in>
2024-07-22 12:57:19 +0530

    fixed issues

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -226,7 +226,7 @@ export class AdminService {
           from: 'admin_users',
           localField: 'oemId',
           foreignField: 'userName',
-          as: 'adminUserDetails'
+          as: 'employeeCompanyDetails'
         }
       }
     ]);
