ed82dea803fce88a57be04984dd4037bb4b7a188
www.abcom.in <www.abcom.in@wwwabcomins-MacBook-Pro.local>
2024-08-14 13:19:24 +0530

    changes in partner producct

--- src/services/admin.service.ts
+++ src/services/admin.service.ts
@@ -34,7 +34,7 @@ export class AdminService {
     TYPES.SurepassService
   );
 
-  async create(reqBody: IAdmin): Promise<IAdmin> {
+  async create(reqBody: any): Promise<IAdmin> {
     const upAdminFields = Object.assign({}, reqBody) as IAdmin;
 
     // Update the password
@@ -56,6 +56,11 @@ export class AdminService {
     console.log(permissions.OEM, 'f;klmk');
     upAdminFields.accessList = permissions.OEM;
     console.log(upAdminFields, 'fw;elk');
+
+    if (reqBody?.documents?.gstData?.business_name) {
+      upAdminFields.documents.gstData.businessName =
+        reqBody?.documents?.gstData?.business_name;
+    }
 
     const newAdmin: IAdmin = (
       await Admin.create(upAdminFields)
@@ -116,7 +121,7 @@ export class AdminService {
     return res;
   }
 
-  async updateUser(reqBody: IAdmin, userName: string): Promise<any> {
+  async updateUser(reqBody: any, userName: string): Promise<any> {
     const user = await this.getAdminUserByUserName(userName);
     if (!user) {
       Logger.error('<Service>:<AdminService>:<Admin User not found>');
@@ -131,6 +136,10 @@ export class AdminService {
         reqBody.updateCount ? Number(reqBody.updateCount) + 1 : 1
       );
       reqBody.status = 'DISABLED';
+    }
+    if (reqBody?.documents?.gstData?.gstin) {
+      reqBody.documents.gstData.businessName =
+        reqBody?.documents?.gstData?.business_name;
     }
     Logger.debug(`${query.updateCount},${reqBody.userName}, ${reqBody}`);
     const res = await Admin.findOneAndUpdate(query, reqBody, {
