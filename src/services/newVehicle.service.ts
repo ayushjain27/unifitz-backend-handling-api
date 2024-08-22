7613add38924cbe73a21e0ae73591f4692463ab0
www.abcom.in <www.abcom.in@wwwabcomins-MacBook-Pro.local>
2024-08-22 15:55:41 +0530

    changes in vehicle

--- src/services/newVehicle.service.ts
+++ src/services/newVehicle.service.ts
@@ -145,12 +145,42 @@ export class NewVehicleInfoService {
       _id: vehicleID
     })?.lean();
 
+    const jsonData = {
+      ...vehicleResult,
+      colorCode: vehicleResult?.colorCode?.map((val, key) =>
+        key === 0
+          ? {
+              ...val,
+              image: vehicleResult?.documents?.vehicleImageList?.first?.docURL
+            }
+          : key === 1
+          ? {
+              ...val,
+              image: vehicleResult?.documents?.vehicleImageList?.second?.docURL
+            }
+          : key === 2
+          ? {
+              ...val,
+              image: vehicleResult?.documents?.vehicleImageList?.third?.docURL
+            }
+          : key === 3
+          ? {
+              ...val,
+              image: vehicleResult?.documents?.vehicleImageList?.fourth?.docURL
+            }
+          : {
+              ...val,
+              image: vehicleResult?.documents?.vehicleImageList?.fifth?.docURL
+            }
+      )
+    };
+
     if (_.isEmpty(vehicleResult)) {
       throw new Error('vehicle does not exist');
     }
     Logger.info('<Service>:<vehicleService>:<Upload vehicle successful>');
 
-    return vehicleResult;
+    return jsonData;
   }
 
   async update(reqBody: any, vehicleId: string): Promise<any> {
