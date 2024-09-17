ed6d5be80ac6aaa353166b80c8818afa2c8914bb
AayushJain27 <ayush@serviceplug.in>
2024-09-17 18:03:12 +0530

    fixed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -33,7 +33,9 @@ export const surepassConfig: SurepassConfig = {
 
 export const testUsers = [
   { phoneNo: '7777777777', role: 'STORE_OWNER', otp: '7777' },
-  { phoneNo: '9999999999', role: 'USER', otp: '9999' }
+  { phoneNo: '9999999999', role: 'USER', otp: '9999' },
+  { phoneNo: '3344553455', role: 'STORE_OWNER', otp: '654321' },
+  { phoneNo: '3344553455', role: 'USER', otp: '654321' },
 ];
 
 export const serverkey = "AAAAw_xRwT0:APA91bHRGVoe2i4Mnu-2D6ixCDXm9E68WNmYu9SFhx_tsmhgZkOSLr7GWKTOnLnw4pbRAgWLkyaoRLs2dD6LBVI2PvVCHTEkKWl3PQnOFrXkh1DE0ihwcalXx2K9-bm64oINV5xVA2Fz"
