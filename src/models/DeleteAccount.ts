c74c513186d13bd283ff59c2faf4cf2044bdcb71
Jitin Rathi <jitinrathi910949@gmail.com>
2024-10-03 15:43:10 +0530

    delete account model prepare

--- src/models/DeleteAccount.ts
+++ src/models/DeleteAccount.ts
@@ -4,18 +4,49 @@ import { model, Schema } from 'mongoose';
  * Interface to model the Customer Schema for TypeScript.
  */
 export interface IDeleteAccount {
+  feedback: string[];
+  comments?: string;
   createdAt?: Date;
   updatedAt?: Date;
+  userId: string;
+  app: string;
+  storeId?: string;
+  customerId?: string;
 }
 
 const deleteAccountSchema: Schema = new Schema(
   {
-    
+    feedback: {
+      type: [String],
+      required: true
+    },
+    comments: {
+      type: String,
+      required: false
+    },
+    userId: {
+      type: String,
+      required: true
+    },
+    app: {
+      type: String,
+      required: true
+    },
+    storeId: {
+      type: String,
+      required: false
+    },
+    customerId: {
+      type: String,
+      required: false
+    }
   },
-  { timestamps: true }
+  { timestamps: true, strict: false }
 );
 
-
-const DeleteAccount = model<IDeleteAccount>('deleteAccount', deleteAccountSchema);
+const DeleteAccount = model<IDeleteAccount>(
+  'deleteAccount',
+  deleteAccountSchema
+);
 
 export default DeleteAccount;
