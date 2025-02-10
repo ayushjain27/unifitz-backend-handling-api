0815ee09431287ab0c849aae703192a6bcc45813
AayushJain27 <ayush@serviceplug.in>
2025-02-10 12:33:18 +0530

    new updates of notification

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -6,11 +6,18 @@ import Logger from '../config/winston';
 import { firebaseAdmin } from '../config/firebase-config';
 import { messaging } from 'firebase-admin';
 import { App } from 'firebase-admin/app';
+import container from '../config/inversify.container';
+import { TYPES } from '../config/inversify.types';
+import Store from '../models/Store';
+import { SQSService } from './sqs.service';
+import { SQSEvent } from '../enum/sqsEvent.enum';
+import Customer from '../models/Customer';
 const FCM = require('fcm-node');
 // import Customer, { ICustomer } from './../models/Customer';
 
 @injectable()
 export class NotificationService {
+  private sqsService = container.get<SQSService>(TYPES.SQSService);
   // async sendNotification(params: any): Promise<any> {
   //   Logger.info(
   //     '<Service>:<NotificationService>: <Sending notification: sending notfication to user>'
@@ -37,38 +44,37 @@ export class NotificationService {
     Logger.info(
       '<Service>:<NotificationService>: <Sending notification: sending notfication to user>'
     );
-    const serverKey =
-      'AAAAw_xRwT0:APA91bHRGVoe2i4Mnu-2D6ixCDXm9E68WNmYu9SFhx_tsmhgZkOSLr7GWKTOnLnw4pbRAgWLkyaoRLs2dD6LBVI2PvVCHTEkKWl3PQnOFrXkh1DE0ihwcalXx2K9-bm64oINV5xVA2Fz';
-    const fcm = new FCM(serverKey);
 
-    const message = {
-      notification: {
-        title: 'Notification Title',
-        body: 'Notification Body'
-      },
-      to: 'f1n6CAd6ScSXA9uVCeJ-Z-:APA91bHTX9lTvwanm3N7V7Z8c7Q61ePvbXq33a1ZR6ZOxhapUZJBMnInW-46DAQfMdBfg0NIwx6zfvXa603H3527UcrhFcxWwWC2MHz0j0A61oeNVRamgQB0Z5yAXh3Fk1Ly4UFkp00R'
-    };
-
-    // const registrationToken = params.fcmToken;
-    // const payload = params.payload;
-    // const options = {
-    //   priority: 'high',
-    //   timeToLive: 60 * 60 * 24
-    // };
     try {
-      // const response = await messaging(firebaseAdmin as App | any).sendToDevice(
-      //   registrationToken,
-      //   payload,
-      //   options
-      // );
-      // return response;
-      fcm.send(message, function (err: any, response: any) {
-        if (err) {
-          console.log('Error', err);
-        } else {
-          console.log('Response', response);
-        }
-      });
+      let partners = await Customer.find({});
+      for (const partner of partners) {
+        // await sendNotification(
+        //   '🚗 New Features Alert! 🚗',
+        //   'Buy and Sell Vehicles Easier Than Ever. Explore our latest updates to find your perfect ride or sell yours quickly. Check it out now!',
+        //   customer?.phoneNumber,
+        //   'USER',
+        //   ''
+        // );
+        // const data = {
+        //   title: 'New Feature',
+        //   body: 'Introducing Buy Spares: Purchase spare parts directly from distributors at your convenience.',
+        //   phoneNumber: partner?.contactInfo?.phoneNumber?.primary,
+        //   role: 'STORE_OWNER',
+        //   type: 'BUY_SPARES'
+        // };
+        const data = {
+          title: 'Buy/ Renewal Insurane!',
+          body: 'SMC Insurance is here! Protect your vehicle with affordable and reliable insurance plans.',
+          phoneNumber: partner?.phoneNumber,
+          role: 'USER',
+          type: 'SMC_INSURANCE',
+        };
+        console.log(data,"data send");
+        const sqsMessage = await this.sqsService.createMessage(
+          SQSEvent.NOTIFICATION,
+          data
+        );
+      }
     } catch (err) {
       throw new Error(err);
     }
