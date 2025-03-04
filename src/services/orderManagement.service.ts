b0c4d3acd28a1ef0a979a46ac07fa2e7c98dc3e5
AayushJain27 <ayush@serviceplug.in>
2025-03-04 10:33:22 +0530

    modify api

--- src/services/orderManagement.service.ts
+++ src/services/orderManagement.service.ts
@@ -1758,6 +1758,7 @@ export class OrderManagementService {
         {
           $match: { customerId: sparePostId }
         },
+        { $sort: { createdAt: -1 } },
         {
           $skip: pageNo * pageSize
         },
